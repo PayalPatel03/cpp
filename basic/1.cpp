a0c2af7100c195cfc9845cfe1a3d9b06b16a1e06
unknown <p6579113@gmail.com>
2024-11-15 21:52:02 +0530

    done

---
+++ basic/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    cout << "Hello world" <<endl;
+    
+    int a;
+    int b;
+
+    cout<<"Enter value of a" ;
+    cin>>a;
+    
+
+    cout<<"Enter value of b";
+    cin>>b;
+
+    int c=a+b;
+    cout << "Addition of a and b is:"<< c;
+    
+
+    return 0;
+}
