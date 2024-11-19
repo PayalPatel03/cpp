d5d990ec6af7eed4e60a36e3b8adfed1d3d26a41
unknown <p6579113@gmail.com>
2024-11-19 17:53:49 +0530

    project-1

---
+++ simple_gradecalc/q1.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m1,m2,m3;
+    int total;
+    char grade;
+    float avg;
+
+    cout << "Enter your score of first subject:" << endl;
+    cin >> m1;
+    cout << "Enter your score of second subject:" << endl;
+    cin >> m2;
+    cout << "Enter your score of third subject:" << endl;
+    cin >> m3;
+
+    total=m1+m2+m3;
+    cout << "Total is :" << total << endl ;
+
+    avg = total / 3;
+    cout << "Average is :" << avg << endl;
+
+    //USing ternary operator
+
+        grade = avg >= 90 ? 'A' : avg >= 80 ? 'B' : avg >= 70 ? 'C' : avg >= 60 ? 'D' : 'F';
+
+        cout << "Your garde is " << grade;    
+
+    //Applying switch case
+
+    switch(grade){
+        case 'A' : cout << ".Excellent work!!" ;
+        break;
+        case 'B' : cout << ".Well done!!";
+        break;
+        case 'C' : cout << ".Good Job" ;
+        break;
+        case 'D' : cout << ".You are passed, but you could do better." ;
+        break;
+        default : cout << ".You have failed the examination" ;
+    }
+    // Eligiblity criteria 
+
+    if((grade == 'A') || (grade == 'B') || (grade == 'C') || (grade == 'D')){
+        cout << "Congratulations! You are eligible for the next level" << endl;
+    }
+    else{
+        cout << "Plese try again next time" ;
+    }
+  				 
+    
+
+
+
+
+
+
+
+    
+
+}
