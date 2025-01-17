449aa3f4211adbd590205e355767df21bee595e4
unknown <p6579113@gmail.com>
2025-01-17 17:04:17 +0530

    done

---
+++ Banking System/bank.cpp
@@ -0,0 +1,1 @@
+//Devlop a simple banking system that demonstates the concept of polymorphism,encuplation and inheritance.The sysytem allow users to create diffent typesof bank accounts,performnaces,and retrive account information.
